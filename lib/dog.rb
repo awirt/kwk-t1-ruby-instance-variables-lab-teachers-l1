eb9d93f7118175c8b5eca7edba14d4c46e77e207
Andrew Wirt <awjwirt@gmail.com>
2018-07-01 20:49:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,11 @@
 class Dog
   
-  def name= #reading the name
-    @this_dogs_name = 
+  def name=(dog_name) #writing the name
+    @this_dogs_name = dog_name
+  end
+  
+  def name #reading the name
+    @this_dogs_name
   end
   
 end
